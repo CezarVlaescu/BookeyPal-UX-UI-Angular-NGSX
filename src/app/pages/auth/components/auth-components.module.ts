c23e58b0f55458e7a39ea68372d14a3408b5707f
Vlaescu Cezar <108024461+CezarVlaescu@users.noreply.github.com>
2024-04-07 20:50:12 +0300

    Add files via upload

---
+++ src/app/pages/auth/components/auth-components.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthComponentsRoutingModule } from './auth-components-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AuthComponentsRoutingModule
+  ]
+})
+export class AuthComponentsModule { }
---
+++ src/app/shared/directives/auth-directive.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appAuthDirective]'
+})
+export class AuthDirectiveDirective {
+
+  constructor() { }
+
+}
